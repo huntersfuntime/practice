e0b1ab76dad2f73f87e41346f84956dd7515d36b
hunterorges <hunterorges@gmail.com>
2018-12-05 19:45:05 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      body: 'content here',
+      customerIds: [{id: 1, name: 'Gary'}],
+      filter: 'individual',
+      subject: 'some random subject',
+      customerStatus: [{id: 'active_payment', name: 'Active'}],
+      customerList: '',
+      customerStatuses: ''
+    }
+
+    // this is the props being brough in
+
+    this.customerIndividuals = [['Gary', 1], ['Larry', 2], ['Hary', 3]]
+    this.customerGroups = [['Active', 'active'], ['Past Due', 'past_due']]
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let customer_notification_batch = {
+      body: this.state.body,
+      customer_status: this.state.customerStatus,
+      filter: this.state.filter,
+      subject: this.state.subject,
+      customer_ids: this.state.customerIds
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Test
           </p>
           <a
             className="App-link"
@@ -24,5 +51,12 @@ class App extends Component {
     );
   }
 }
+// form data needed
+// body: string
+// customer_status: string ('active')   
+// filter: this.state.filter, string ('group')
+// subject: this.state.subject, string ('subject')
+// customer_ids: this.state.customerIds, array [1, 2, 3]
+
 
 export default App;
