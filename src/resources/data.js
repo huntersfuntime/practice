cdb83dad4e043f943f0e0d3daf8c8d2defa1f8b4
tyler gilland <tylergilland@gmail.com>
2018-12-05 23:15:00 -0700

    *waved the elder wand*

---
+++ src/resources/data.js
@@ -0,0 +1,5 @@
+export const groups = [["Active", "active"], ["Past Due", "past_due"]]
+export const active_count = 5
+export const past_due_count = 10
+
+export const customers = [["Gary", 1], ["Larry", 2], ["Hary", 3]]
